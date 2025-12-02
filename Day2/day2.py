e3d40fbdfd7622133befcc40ff23b079be0b2689
Raptor200111 <alvardalsva@gmail.com>
2025-12-02 22:14:07 +0100

    day 2, part 2

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,3 +1,12 @@
+# Source - https://stackoverflow.com/questions/3844801/check-if-all-elements-in-a-list-are-equal
+# Posted by kennytm, modified by community. See post 'Timeline' for change history
+# Retrieved 2025-12-02, License - CC BY-SA 4.0
+from itertools import groupby
+def all_equal(iterable):
+    g = groupby(iterable)
+    return next(g, True) and not next(g, False)
+#
+
 def invalidIdPart1(line: str) -> int:
 
     sum = 0
@@ -19,16 +28,36 @@ def invalidIdPart1(line: str) -> int:
 
     return sum
 
-def invalidIdPart2(f):
-    nz = 0
-    
+def invalidIdPart2(line: str) -> int:
+    sum = 0
+    line = line.split(',')
 
-    return nz
+    for ran in line:
+        ranges = ran.split('-')
+        inital = int(ranges[0])
+        end = int(ranges[1])
+        i = inital
+        while (i <= end):
+            for mult in range(2, len(str(i)) + 1):
+                if (len(str(i)) % mult == 0):
+                    listy = []
+                    for m in range(mult):
+                        listy.append(str(i)[int(m*int(len(str(i))/mult)) : int((m+1)*int(len(str(i))/mult))])
+                    #print(str(mult) + " " + str(m))
+                    #print(listy)
+
+                    if all_equal(listy.__iter__()):
+                        sum += i
+                        break
+                    
+            i += 1
+
+    return sum
 
 
 f = open("input.txt")
 #f = open("test.txt")
 
-print(invalidIdPart1(f.read())) #result test: 1227775554, input: 
+#print(invalidIdPart1(f.read())) #result test: 1227775554, input: 9188031749
 
-#print(invalidIdPart2(f)) #result test: 6, input: 6738
+print(invalidIdPart2(f.read())) #result test: 4174379265, input: 11323661261
