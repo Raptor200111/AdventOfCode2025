7a36269776771a6388bce6f67bd053398a86de91
Raptor200111 <alvardalsva@gmail.com>
2025-12-05 19:41:49 +0100

    day 5, part 2

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,6 +1,7 @@
 
 from ctypes import Array
 from io import TextIOWrapper
+from typing import List
 
 def LineToRange(line: str) -> Array:
     line = line.split('-')
@@ -21,8 +22,7 @@ def key_sum(r):
 def NumberWithinRange(n: int, r:Array) -> True:
     return n >= r[0] and n <= r[1]
 
-def FreshnessPart1(f: TextIOWrapper):
-    sum = 0
+def orderRanges(f: TextIOWrapper) -> List:
     ranges = []
 
     line = f.readline().strip('\n')
@@ -50,6 +50,11 @@ def FreshnessPart1(f: TextIOWrapper):
                 ranges.sort(key=key_sum)
         
         line = f.readline().strip('\n')
+    return ranges
+
+def FreshnessPart1(f: TextIOWrapper):
+    sum = 0
+    ranges = orderRanges(f)
 
     #print(ranges)
     line = f.readline().strip('\n')
@@ -62,12 +67,17 @@ def FreshnessPart1(f: TextIOWrapper):
         line = f.readline().strip('\n')
     return sum
 
-def FreshnessPart2(f):
+def FreshnessPart2(f: TextIOWrapper):
     sum = 0
+    ranges = orderRanges(f)
+
+    #print(ranges)
+    for r in ranges:
+        sum += r[1] - r[0] + 1
     return sum
 
 f = open("input.txt")
 #f = open("test.txt")
 
-print(FreshnessPart1(f)) #result test: 3, input: 720
-#print(FreshnessPart2(f)) #result test: , input: 
+#print(FreshnessPart1(f)) #result test: 3, input: 720
+print(FreshnessPart2(f)) #result test: 14, input: 
