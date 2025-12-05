9d0baa6342df733f7b15c15fb38dbf48f0660bc1
Raptor200111 <alvardalsva@gmail.com>
2025-12-05 11:03:46 +0100

    day 4, part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -43,14 +43,45 @@ def PaperCountPart1(matrix):
                     sum += 1
     return sum
 
-def PaperCountPart2(f):
+def PaperCountPart2(matrix):
     sum = 0
-    for line in f:
-        sum += 1
+    height = len(matrix)
+    width = len(matrix[0])
+
+    removedSomePaper = True
+
+    while removedSomePaper:
+        minisum = 0
+        for j in range(height):
+            for i in range(width):
+                if (matrix[j][i] == '@'):
+                    count = 0
+                    if (validMatrixCoords(i-1,j-1,width,height) and matrix[j-1][i-1] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i,j-1,width,height) and matrix[j-1][i] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i+1,j-1,width,height) and matrix[j-1][i+1] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i-1,j,width,height) and matrix[j][i-1] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i+1,j,width,height) and matrix[j][i+1] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i-1,j+1,width,height) and matrix[j+1][i-1] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i,j+1,width,height) and matrix[j+1][i] == '@'):
+                        count += 1
+                    if (validMatrixCoords(i+1,j+1,width,height) and matrix[j+1][i+1] == '@'):
+                        count += 1
+                    if (count < 4):
+                        minisum += 1
+                        matrix[j][i] = '.'
+        sum += minisum
+        if minisum == 0:
+            removedSomePaper = False
     return sum
 
 f = open("input.txt")
 #f = open("test.txt")
 
-print(PaperCountPart1(matrixFromFile(f))) #result test: 13, input: 1428
-#print(PaperCountPart2(f)) #result test: , input: 
+#print(PaperCountPart1(matrixFromFile(f))) #result test: 13, input: 1428
+print(PaperCountPart2(matrixFromFile(f))) #result test: 43, input: 8936
