55f669cea233eb87f08e92c78e9139f74633c31c
Raptor200111 <alvardalsva@gmail.com>
2025-12-07 17:22:31 +0100

    day 6, part 2

--- Day6/day6.py
+++ Day6/day6.py
@@ -32,10 +32,71 @@ def CephalopodMathPart1(f):
 
 def CephalopodMathPart2(f):
     sum = 0
+    matrix = []
+
+    line = f.readline().strip('\n')
+
+    while line != "":
+        if line[0] == '*' or line[0] == '+':
+            break
+        matrix.append(line)
+        line = f.readline().strip('\n')
+
+    signs = line
+    lastSign = ' '
+    numbers = []
+    for i in range(len(matrix[0])):
+        if signs[i] == '*' or signs[i] == '+':
+            lastSign = signs[i]
+        
+        allSpaces = True
+
+        for j in range(len(matrix)):
+            if matrix[j][i] != ' ':
+                allSpaces = False
+                break
+
+        if allSpaces:
+            summ = 0
+            if lastSign == '*':
+                for n in numbers:
+                    if summ == 0:
+                        summ = n
+                    else:
+                        summ *= n
+            elif lastSign == '+':
+                for n in numbers:
+                    summ += n
+            sum += summ
+            numbers = []
+        else:
+            num = 0
+            for j in range(len(matrix)):
+                if matrix[j][i] != ' ':
+                    num *= 10
+                    num += int(matrix[j][i])
+            numbers.append(num)
+
+        if i == len(matrix[0])-1:
+            summ = 0
+            if lastSign == '*':
+                for n in numbers:
+                    if summ == 0:
+                        summ = n
+                    else:
+                        summ *= n
+            elif lastSign == '+':
+                for n in numbers:
+                    summ += n
+            sum += summ
+            numbers = []
+
+
+    #print(matrix)
     return sum
 
 f = open("input.txt")
 #f = open("test.txt")
 
-print(CephalopodMathPart1(f)) #result test: 4277556, input: 
-#print(CephalopodMathPart2(f)) #result test: , input: 
+#print(CephalopodMathPart1(f)) #result test: 4277556, input: 5322004718681
+print(CephalopodMathPart2(f)) #result test: 3263827, input: 9876636978528
