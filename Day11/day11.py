a04fea7118698eb066805987b8b6fabffaf8c7f4
Raptor200111 <alvardalsva@gmail.com>
2025-12-19 23:48:22 +0100

    day 11, part 2

--- Day11/day11.py
+++ Day11/day11.py
@@ -15,7 +15,6 @@ def RecursiveSearchWithMemo(idx, connections, memo):
     return sum
 
 def ReactorPart1(f: TextIOWrapper):
-    
     connections = {}
     for line in f:
         
@@ -25,12 +24,41 @@ def ReactorPart1(f: TextIOWrapper):
     memo = {}
     return RecursiveSearchWithMemo("you", connections, memo)
 
-def ReactorPart2(f):
-    return 0
+def RecursiveSearchWithMemo2(idx: str, connections: dict, memo: dict, dacFound: bool, fftFound: bool):
+    if idx == "dac":
+        dacFound = True
+    if idx == "fft":
+        fftFound = True
+
+    if idx == "out":
+        return 1 if dacFound and fftFound else 0
+    
+    key = (idx, dacFound, fftFound)
+    if key in memo:
+        return memo[key]
+
+    sum = 0
+    for way in connections[idx]:
+        sum += RecursiveSearchWithMemo2(way, connections, memo, dacFound, fftFound)
+
+    memo[key] = sum
+    return sum
+
+def ReactorPart2(f: TextIOWrapper):
+    connections = {}
+    for line in f:
+        line = line.strip().split(':')
+        connections.update({line[0]: line[1].strip().split(' ')})
+
+    memo = {}
+    return RecursiveSearchWithMemo2("svr", connections, memo, False, False)
+    
 
 
 f = open("input.txt")
 #f = open("test.txt")
+#f = open("test2.txt")
 
-print(ReactorPart1(f)) #result test: 5, input: 
-#print(ReactorPart2(f)) #result test: , input: 
+
+#print(ReactorPart1(f)) #result test: 5, input: 494
+print(ReactorPart2(f)) #result test: 2, input: 296006754704850
